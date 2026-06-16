8beb0150c50f163c78820f5245c549e83b84e731
JoeCosta7 <joedc3711@gmail.com>
2026-06-15 21:22:47 -0400

    authorization headers

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPAuthorizationCredentials
 from fastapi.responses import JSONResponse, Response
+from typing import Annotated
 import redis
+import secrets
 from database import Event, EventBase, engine, create_db_and_tables
 from sqlmodel import Session, select
 from metrics import EVENTS, DELIVERED, FAILED, LAST_DELIVERY_DURATION, RETRYING
 import uuid
+import os
 from prometheus_client import generate_latest,CONTENT_TYPE_LATEST
 
 r = redis.Redis(host='redis', port=6379, decode_responses=True)
@@ -17,13 +21,24 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+security = HTTPBasic()
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
 @app.post("/v1/events")
-async def create_event(event: EventBase):
+async def create_event(event: EventBase, credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)]):
+    current_API_key_bytes = credentials.credentials.encode("utf8")
+    correct_API_key_bytes = os.getenv("API_KEY").encode("utf8")
+    is_correct_api_key = secrets.compare_digest(current_API_key_bytes, correct_API_key_bytes)
+    if not is_correct_api_key:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect API KEY",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     newEvent = Event(
         destination_url=event.destination_url,
         event_type=event.event_type,
