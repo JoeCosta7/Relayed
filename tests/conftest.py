b664f92d9b3c5990de239b95b208b312feb16276
JoeCosta7 <joedc3711@gmail.com>
2026-06-23 21:04:18 -0400

    separate testing database from "production" database

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,20 @@
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+load_dotenv(Path(__file__).parent / ".env.test", override=True)
+
 import pytest
-from sqlmodel import Session
+from sqlmodel import Session, SQLModel
 from sqlalchemy import text
 from database import engine
 
-@pytest.fixture(autouse=True)
-def clean_db():
-    # Run before each test
-    with Session(engine) as session:
-        session.execute(text("TRUNCATE TABLE deadletter, event RESTART IDENTITY CASCADE"))
-        session.commit()
+@pytest.fixture(scope="session", autouse=True)
+def setup_schema():
+    SQLModel.metadata.create_all(engine)
     yield
+
+@pytest.fixture(autouse=True)
+def clean_tables():
+    with Session(engine) as session:
+        session.exec(text("TRUNCATE TABLE deadletter, event RESTART IDENTITY CASCADE"))
+        session.commit()
